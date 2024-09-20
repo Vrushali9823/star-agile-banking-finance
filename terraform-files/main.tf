5afbbe3069404773b6f8c92827cad1cfe03b114c
Vrushali Narendra Mange <91605771+Vrushali9823@users.noreply.github.com>
2024-09-20 18:04:57 +0530

    Add files via upload

---
+++ terraform-files/main.tf
@@ -0,0 +1,24 @@
+resource "aws_instance" "test-server" {
+  ami = "ami-0522ab6e1ddcc7055"
+  instance_type = "t2.micro"
+  key_name = "Vrushu12"
+  vpc_security_group_ids = ["sg-0347f95fec51160da"]
+  connection {
+     type = "ssh"
+     user = "ubuntu"
+     private_key = file("./Vrushu12.pem")
+     host = self.public_ip
+     }
+  provisioner "remote-exec" {
+     inline = ["echo 'wait to start the instance' "]
+  }
+  tags = {
+     Name = "test-server"
+     }
+  provisioner "local-exec" {
+     command = "echo ${aws_instance.test-server.public_ip} > inventory"
+     }
+  provisioner "local-exec" {
+     command = "ansible-playbook /var/lib/jenkins/workspace/Banking-Project/terraform-file/ansibleplaybook.yml"
+     }
+  }
